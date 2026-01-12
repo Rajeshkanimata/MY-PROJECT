40bd01c2b7b3ca74284d0ad958e76f371cbd4ae7
rajeshkanimata <rajeshkanimata123@gmail.com>
2026-01-12 13:30:29 +0530

    updates

--- TERRAFORM/VPC/variables.tf
+++ TERRAFORM/VPC/variables.tf
@@ -1,4 +1,22 @@
-# ... (previous variables for vpc_cidr, azs, etc.)
+variable "vpc_cidr" {
+  description = "The IPv4 CIDR block for the VPC"
+  type        = string
+}
+
+variable "azs" {
+  description = "A list of availability zones in the region"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "A list of private subnets inside the VPC"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "A list of public subnets inside the VPC"
+  type        = list(string)
+}
 
 variable "database_subnets" {
   description = "A list of database subnets"
